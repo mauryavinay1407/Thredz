d8a63f15ef9498795dfa7cea32b0b295a1bd221a
mauryavinay1407 <vinaymaurya1407@gmail.com>
2025-03-31 21:26:04 +0530

    fix: prod issue

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 // config.js
 export const BASE_URL = import.meta.env.DEV
   ? "http://localhost:4000"  // Local backend
-  : "https://thredz.onrender.com/"; // Deployed backend
+  : "https://thredz.onrender.com"; // Deployed backend
